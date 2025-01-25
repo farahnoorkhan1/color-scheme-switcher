4326f2c1cb488c4a18b304848f36a2b739e5c271
farahnoorkhan1 <noorrakshi2@gmail.com>
2025-01-25 12:50:42 +0000

    styling changes done

--- public/script.js
+++ public/script.js
@@ -4,13 +4,13 @@ const body = document.querySelector("body");
 buttons.forEach(function(button){
     button.addEventListener('click', function(e){
         switch(e.target.id){
-            case 'grey': body.style.backgroundColor = e.target.id
+            case 'grey': body.style.background = e.target.id
             break
-            case 'white': body.style.backgroundColor = e.target.id
+            case 'white': body.style.background = e.target.id
             break
-            case 'blue': body.style.backgroundColor = e.target.id
+            case 'blue': body.style.background = e.target.id
             break
-            case 'yellow': body.style.backgroundColor = e.target.id
+            case 'yellow': body.style.background = e.target.id
             break
             default: 
             return
